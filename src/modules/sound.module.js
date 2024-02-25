bd15c153a433f7f655fa4f700c433e72cbdf7931
Dmitrii Ilichev <dsilichev@gmail.com>
2024-02-25 21:30:19 +0300

    move playRandomSound to utils

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -1,4 +1,5 @@
-import { Module } from '../core/module'
+import { Module } from '../core/module';
+import { playRandomSound } from '../utils';
 
 export class SoundModule extends Module {
   constructor(type, text) {
@@ -6,26 +7,7 @@ export class SoundModule extends Module {
   }
 
   trigger() {
-    this.playRandomSound();
-  }
-
-  playRandomSound() {
-    let waveforms = ["sine", "square", "sawtooth", "triangle"];
-
-    const context = new AudioContext();
-
-    let oscillatorNode = context.createOscillator();
-    let gainNode = context.createGain();
-
-    oscillatorNode.type = waveforms[Math.floor(Math.random() * waveforms.length)];
-
-    let frequency = (100 + Math.random() * 10000).toFixed(2);
-    oscillatorNode.frequency.value = frequency;
-
-    gainNode.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + .3);
-    oscillatorNode.connect(gainNode);
-    gainNode.connect(context.destination);
-    oscillatorNode.start(0);
+    playRandomSound();
   }
 
   toHTML () {
--- src/utils.js
+++ src/utils.js
@@ -11,3 +11,22 @@ export function getRandomColor() {
   return color;
 }
 
+export function playRandomSound() {
+  let waveforms = ["sine", "square", "sawtooth", "triangle"];
+
+  const context = new AudioContext();
+
+  let oscillatorNode = context.createOscillator();
+  let gainNode = context.createGain();
+
+  oscillatorNode.type = waveforms[Math.floor(Math.random() * waveforms.length)];
+
+  let frequency = (100 + Math.random() * 10000).toFixed(2);
+  oscillatorNode.frequency.value = frequency;
+
+  gainNode.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + .3);
+  oscillatorNode.connect(gainNode);
+  gainNode.connect(context.destination);
+  oscillatorNode.start(0);
+}
+
